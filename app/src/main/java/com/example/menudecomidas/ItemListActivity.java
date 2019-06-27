bb4bc897f29060eb58ef89dc6029bee7f8ec9b58
Alberto Vazquez <albertovazquez120.av@gmail.com>
2019-06-27 17:09:39 -0600

    1

--- app/src/main/java/com/example/menudecomidas/ItemListActivity.java
+++ app/src/main/java/com/example/menudecomidas/ItemListActivity.java
@@ -4,19 +4,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.menudecomidas.dummy.DummyContent;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * An activity representing a list of Items. This activity
@@ -28,6 +36,13 @@ import java.util.List;
  */
 public class ItemListActivity extends AppCompatActivity {
 
+    private static final String PATH_FOOD = "food";
+    @BindView(R.id.etName)
+    EditText etName;
+    @BindView(R.id.etPrice)
+    EditText etPrice;
+    @BindView(R.id.btnSave)
+    Button btnSave;
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
@@ -38,6 +53,7 @@ public class ItemListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+        ButterKnife.bind(this);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -69,6 +85,20 @@ public class ItemListActivity extends AppCompatActivity {
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
     }
+
+    @OnClick(R.id.btnSave)
+    public void onViewClicked() {
+
+        DummyContent.Comida comida = new DummyContent.Comida(etName.getText().toString().trim(),
+                etPrice.getText().toString().trim());
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference(PATH_FOOD);
+        ref.push().setValue(comida);
+        etName.setText("");
+        etPrice.setText("");
+
+    }
+
 
     public static class SimpleItemRecyclerViewAdapter
             extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
